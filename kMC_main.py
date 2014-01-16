05450826f5bfe765ad4a90758b323af87b444d29
jorgehog <jorgen_da_mad@hotmail.com>
2014-01-16 11:50:31 +0100

    set up initial structure: ready to play

---
+++ kMC_main.py
@@ -0,0 +1,35 @@
+#Include numerical python methods, keeping the 'np' namespace 
+#to avoid conflicts in names
+import numpy as np
+
+#Include plot tools (e.g. pylab.plot(x, y, .....))
+#Again, we keep the namespace (if not we can't make a function called e.g. 'plot')
+import matplotlib as mpl
+from matplotlib import pylab
+
+#Standard python modules for reading commandlines and so on..
+import sys, os
+
+
+def kMC():
+    "Do something"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#An if-test that executes the kMC function if and only if it is used 
+#through this scope (to avoid it lauching if included in another file)
+if __name__=="__main__":
+    kMC()       

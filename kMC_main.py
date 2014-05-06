ba30d1187f827b3e1ed55c283aed656c8a52f4d3
jorgehog <jorgen_da_mad@hotmail.com>
2014-05-06 14:22:23 +0200

    updates

--- kMC_main.py
+++ kMC_main.py
@@ -10,11 +10,54 @@ from matplotlib import pylab
 #Standard python modules for reading commandlines and so on..
 import sys, os
 
+from math import pi, exp
 
 #Python programmers use to underscore_separator convention usually
 #unlike java / c++ programmers which use a newCapitalLetter convention
-def kMC_main():
-    "Do something"
+class kMC:
+
+    #Declaring the system variables (not necessary, but transparent)
+    x = None
+    y = None
+    system_size = None    
+    T = None
+    
+    C = 1      #prop const stress
+    r_0 = None #length constant stress
+    
+    def __init__(self, size, T, r_0)
+        self.system_size = size
+        self.T = T
+        self.r_0 = r_0
+        
+        z_init_func = lambda x: (np.sin(2*pi*x/3333) + 1)
+        
+        self.x = np.arange(size) #0, 1, 2, 3, 4 ... 
+        z = z_init_func(x)
+        
+        z[-1] = z[0] #Force initial periodicity
+        
+        plot(z)
+        
+        self.z = z
+    
+        
+    def stress(self, r):
+        return C*exp(r/r_0)
+        
+    def chem_pot_diff(self, x):
+        
+        r_s = get_surface(x)
+        
+        dr = r_s - self.z[x] 
+        
+        a_s = 1
+        a_0 = 1
+        
+        return self.T*log(a_s/a_0) - self.stress(dr)
+        
+    
+    
 
 
 
